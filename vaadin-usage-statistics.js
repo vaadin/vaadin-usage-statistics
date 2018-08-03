3cff291a3bda3a1e1df4b466e02d2822c67f42ab
web-padawan <serguey.kulikov@gmail.com>
2018-08-03 10:27:58 +0300

    Remove hardcoded list of known element names

--- vaadin-usage-statistics.js
+++ vaadin-usage-statistics.js
@@ -78,40 +78,6 @@
         }
       };
     };
-
-    get knownVaadinElementNames() {
-      if (!this._knownVaadinElementNames) {
-        const names = [
-          'vaadin-button',
-          'vaadin-checkbox',
-          'vaadin-combo-box',
-          'vaadin-context-menu',
-          'vaadin-date-picker',
-          'vaadin-dialog',
-          'vaadin-dropdown-menu',
-          'vaadin-form-layout',
-          'vaadin-grid',
-          'vaadin-horizontal-layout',
-          'vaadin-icons',
-          'vaadin-item',
-          'vaadin-list-box',
-          'vaadin-notification',
-          'vaadin-password-field',
-          'vaadin-progress-bar',
-          'vaadin-radio-button',
-          'vaadin-split-layout',
-          'vaadin-tab',
-          'vaadin-tabs',
-          'vaadin-text-area',
-          'vaadin-text-field',
-          'vaadin-upload',
-          'vaadin-vertical-layout'
-        ];
-        this._knownVaadinElementNames = names;
-      }
-
-      return this._knownVaadinElementNames;
-    }
     getUsedVaadinElementVersion(elementName) {
       const klass = customElements.get(elementName);
       if (klass) {
