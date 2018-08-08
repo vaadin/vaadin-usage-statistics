933f3f818bb8a5d7350fff1a21ace5789688c68b
web-padawan <serguey.kulikov@gmail.com>
2018-08-08 11:51:39 +0300

    Use npm explore for optout to prevent reinstalling everything

--- check.js
+++ check.js
@@ -1,11 +1,11 @@
+var fs = require('fs');
+
 if (process.env.npm_package_config_disabled) {
   console.log(`
     You have disabled Vaadin development time usage statistics collection. To re-enable, run:
-    npx -p @vaadin/vaadin-usage-statistics enable
+    npm explore @vaadin/vaadin-usage-statistics -- npm run enable
     For more details, see https://github.com/vaadin/vaadin-usage-statistics
   `);
-
-  var fs = require('fs');
 
   try {
     fs.renameSync('vaadin-usage-statistics.js', 'vaadin-usage-statistics.js.bak');
@@ -29,8 +29,31 @@ if (process.env.npm_package_config_disabled) {
 } else {
   console.log(`
     Vaadin collects development time usage statistics to improve this product. To opt-out, run:
-    npx -p @vaadin/vaadin-usage-statistics disable
+    npm explore @vaadin/vaadin-usage-statistics -- npm run enable
     For more details, see https://github.com/vaadin/vaadin-usage-statistics
   `);
+  /* restore backup files if previously disabled */
+  if (fs.existsSync('vaadin-usage-statistics.js.bak')) {
+    try {
+      fs.renameSync('vaadin-usage-statistics.js', 'vaadin-usage-statistics-optout.js');
+    } catch (err) {
+      if (err.code === 'ENOENT') {
+        console.log('File not found!');
+      } else {
+        throw err;
+      }
+    }
+
+    try {
+      fs.renameSync('vaadin-usage-statistics.js.bak', 'vaadin-usage-statistics.js');
+    } catch (err) {
+      if (err.code === 'ENOENT') {
+        console.log('File not found!');
+
+      } else {
+        throw err;
+      }
+    }
+  }
 }
 
